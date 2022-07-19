dea45363d23a7162a24bc43cb5348524d23dc438
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-07-19 16:53:34 +0300

    commit tree ce6cc575b65ceabe22d0848df1dc59068a5368d9

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div className="navbar">
+    <NavLink to={'/'} exact style={{marginRight:'10px'}}>Home</NavLink>
+    <NavLink to={'/movies'} exact style={{marginRight:'10px'}}>Movies</NavLink>
+    <NavLink to={'/directors'} style={{marginRight:'10px'}}>Directors</NavLink>
+    <NavLink to={'/actors'} style={{marginRight:'10px'}}>Actors</NavLink>
+  </div>
+  );
 }
 
 export default NavBar;
